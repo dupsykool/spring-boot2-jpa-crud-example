c94b736160433f402c4a58725021030183c04336
Dupsy <dupsykool>
2019-10-20 23:35:02 +0100

    updates

--- src/main/java/net/guides/springboot2/springboot2jpacrudexample/Application.java
+++ src/main/java/net/guides/springboot2/springboot2jpacrudexample/Application.java
@@ -1,9 +1,10 @@
 package net.guides.springboot2.springboot2jpacrudexample;
 
+import net.guides.springboot2.springboot2jpacrudexample.controller.EmployeeController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = EmployeeController.class)
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);

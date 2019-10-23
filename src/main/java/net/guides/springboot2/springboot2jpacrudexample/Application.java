29bf1e888c3ecae3110d860e8b40335f3f5a6d09
Dupsy <dupsykool>
2019-10-23 08:42:04 +0100

    updates

--- src/main/java/net/guides/springboot2/springboot2jpacrudexample/Application.java
+++ src/main/java/net/guides/springboot2/springboot2jpacrudexample/Application.java
@@ -1,11 +1,18 @@
 package net.guides.springboot2.springboot2jpacrudexample;
 
+import com.netflix.hystrix.contrib.javanica.aop.aspectj.HystrixCommandAspect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public HystrixCommandAspect hystrixAspect(){
+		return new HystrixCommandAspect();
 	}
 }
